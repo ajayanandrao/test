b63eee7967c09d5529c6e4423c285f1b7a1d7bae
Ajay Anandrao <ajayanandrao02@outlook.com>
2023-11-09 16:51:12 +0530

    first commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from './../logo.svg';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h3>Wellcome Ajay sir</h3>
+                </header>
+            </div>
+        </div>
+    )
+}
+
+export default Home
